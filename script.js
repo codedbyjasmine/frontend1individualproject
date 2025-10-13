fa78d2db6231ebb72f968425842dd82f0912adf2
Jasmine Bedhammar <jasmine_bedhammar@hotmail.com>
2025-10-13 17:19:44 +0200

    Styling and JS added

---
+++ script.js
@@ -0,0 +1,40 @@
+const submitPost = document.getElementById('submit-post');
+const usernameInput = document.getElementById('username');
+const postTimeInput = document.getElementById('post-time');
+const titleInput = document.getElementById('post-title');
+const blogInput = document.getElementById('blog-input');
+
+function createPost(post) {
+    const blogPost = document.createElement('div');
+    blogPost.className = 'blog-post';
+
+    const postContent = `
+        <h2>${post.title}</h2>
+        <h4>Författare: ${post.username}</h4>
+        <h4>Datum: ${post.date}</h4>
+        <p>${post.content}</p>
+    `;
+
+    blogPost.innerHTML = postContent;
+
+    document.getElementById('blog-post').appendChild(blogPost);
+}
+
+submitPost.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const post = {
+        username: usernameInput.value,
+        date: postTimeInput.value,
+        title: titleInput.value,
+        content: blogInput.value
+    };
+
+    createPost(post);
+
+    // Clear form fields after submission
+    usernameInput.value = '';
+    postTimeInput.value = '';
+    titleInput.value = '';
+    blogInput.value = '';
+});
